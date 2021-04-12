e5cb32cf7401ec98da586bc3b834b6b937c2b360
lmodenez <lmodenez98@gmail.com>
2021-04-12 20:04:33 -0300

    feat: add ActiveLink component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import Link from 'next/link';
+
 import { SingInButton } from '../SingInButton';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
+
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SingInButton />
